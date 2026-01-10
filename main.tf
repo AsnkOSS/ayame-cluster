3a1463c5eab6e9dc4e55cfad0d8027e55fe5ff18
Asai Neko <sugar@sne.moe>
2026-01-10 23:20:11 +0800

    Add s3 backend
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- main.tf
+++ main.tf
@@ -5,6 +5,21 @@ terraform {
       version = "~> 1.45"
     }
   }
+  backend "s3" {
+    bucket = "asai-terraform-state"
+    key    = "ayame-cluster.tfstate"
+    region = "eu-central-1"
+
+    endpoints = {
+      s3 = "https://fsn1.your-objectstorage.com"
+    }
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
 }
 
 variable "hcloud_token" {
