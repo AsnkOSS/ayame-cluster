f90e3b8d225626582480c9b23ccad56fe632fcb9
Asai Neko <sugar@sne.moe>
2026-01-12 01:08:17 +0800

    Add kubeconfig env to devenv
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ### devenv.nix env
 
+- KUBECONFIG
 - TF_VAR_hcloud_token
 - AWS_ACCESS_KEY_ID
 - AWS_SECRET_ACCESS_KEY
--- devenv.nix
+++ devenv.nix
@@ -19,6 +19,7 @@
   ];
 
   env = {
+    KUBECONFIG = "./secrets/kubeconfig";
     TF_VAR_hcloud_token = "op://Ayame/Hcloud_Token/credential";
     AWS_ACCESS_KEY_ID = "op://Ayame/Hcloud_S3_Access_Key/credential";
     AWS_SECRET_ACCESS_KEY = "op://Ayame/Hcloud_S3_Secret_Key/credential";
